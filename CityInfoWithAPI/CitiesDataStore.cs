64c8f5e651278a73e8625590ff7a58c2a90fa42a
Raluca Gherase <ralucagherase96@gmail.com>
2017-07-13 17:53:36 +0300

    Create Controller for cities and models

---
+++ CityInfoWithAPI/CitiesDataStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfoWithAPI.Models;
+
+namespace CityInfoWithAPI
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; }=new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            //init dummy data
+
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "It has a huge park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "It has a not finished cathedral."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Paris",
+                    Description = "It has Eiffel Tower."
+                }
+            };
+        }
+
+    }
+}
--- CityInfoWithAPI/Startup.cs
+++ CityInfoWithAPI/Startup.cs
@@ -16,6 +16,7 @@ namespace CityInfoWithAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +28,15 @@ namespace CityInfoWithAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
+            {
+                app.UseExceptionHandler();
+            }
+            app.UseMvc();
+            /*app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
-            });
+            });*/
         }
     }
 }
